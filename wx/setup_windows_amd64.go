c2523c2e477a5d99270e15c5123c335d2e0d6bde
Shengqiu Li <lishengqiu.hit@gmail.com>
2017-05-15 00:22:36 +0800

    Add a tag for MSYS64 workaround

--- wx/setup_windows_amd64.go
+++ wx/setup_windows_amd64.go
@@ -2,6 +2,7 @@ package wx
 
 // #cgo CPPFLAGS: -I${SRCDIR}/windows_amd64/ -I${SRCDIR}/../wxWidgets/wxWidgets-3.1.0/include -D_FILE_OFFSET_BITS=64 -D__WXMSW__
 // #cgo !wxgo_binary_package_build LDFLAGS: -L${SRCDIR}/windows_amd64/lib -lwxmsw31u -lwxmsw31u_gl -lwxscintilla -lwxregexu -lwxexpat -lwxtiff -lwxjpeg -lwxpng -lwxzlib 
+// #cgo msys64-workaround LDFLAGS: -Wl,--allow-multiple-definition
 // #cgo LDFLAGS: -l:libstdc++.a -Wl,--subsystem,windows -mwindows -lopengl32 -lglu32 -lrpcrt4 -loleaut32 -lole32 -luuid -lwinspool -lwinmm -lshell32 -lshlwapi -lcomctl32 -lcomdlg32 -ladvapi32 -lversion -lwsock32 -lgdi32 -lntdll -lmsvcrt
 // #cgo CXXFLAGS: -fpermissive
 import "C"
